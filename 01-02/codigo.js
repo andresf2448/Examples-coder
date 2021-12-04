d7c89be935c81d31234bd7c0f3e5b183d1443711
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2021-12-04 17:53:03 -0600

    first commit

---
+++ 01-02/codigo.js
@@ -0,0 +1,54 @@
+/* var numero1 = 2;
+var numero2 = 3;
+
+if(numero1 === 2){
+  console.log('son iguales');
+}
+
+if(numero2 === 3){
+  console.log('hola')
+} */
+
+/* var dinero = 7;
+
+if(dinero < 5){
+  console.log("tienes menos de 5 pesos");
+}else if(dinero == 5){
+  console.log("tienes 5 pesos");
+}else{
+  console.log("tienes más de 5 pesos");
+} */
+
+/* var edad = parseInt(prompt("Ingresa tu edad"));
+
+if(edad <= 12){
+  alert("no puedes entrar");
+}else if(edad > 12 && edad < 18){
+  alert("Puedes entrar con un adulto");
+}else{
+  alert("Puedes entrar");
+} */
+
+
+/* if(dinero < 5){
+  console.log("tienes menos de 5 pesos");
+}
+if(dinero == 5){
+  console.log("tienes 5 pesos");
+
+}
+if(dinero > 5){
+  console.log("tienes más de 5 pesos");
+} */
+
+
+var salir = false;
+var comer = false;
+
+console.log(salir && comer);
+console.log(salir || !comer);
+
+
+
+(true && false && true) || false = false
+true || (false || false) = true
