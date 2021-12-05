ba74785b945143f08544810e99780804ca8dbcd6
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2021-12-04 18:24:03 -0600

    class 01-02

--- 01-02/codigo.js
+++ 01-02/codigo.js
@@ -1,4 +1,15 @@
-/* var numero1 = 2;
+//tipos de variables
+var nombre = "Camila"; //string
+var numero = 4; //number
+var condicion = true; //boolean
+
+//concatenación de cadenas de texto o string
+var cadena1 = "Hola";
+var cadena2 = "Agostina";
+console.log(cadena1 + " " + cadena2);
+
+//condicional simple
+var numero1 = 2;
 var numero2 = 3;
 
 if(numero1 === 2){
@@ -7,9 +18,10 @@ if(numero1 === 2){
 
 if(numero2 === 3){
   console.log('hola')
-} */
+}
 
-/* var dinero = 7;
+//condicionales anidados
+var dinero = 7;
 
 if(dinero < 5){
   console.log("tienes menos de 5 pesos");
@@ -17,9 +29,10 @@ if(dinero < 5){
   console.log("tienes 5 pesos");
 }else{
   console.log("tienes más de 5 pesos");
-} */
+}
 
-/* var edad = parseInt(prompt("Ingresa tu edad"));
+//condicionales anidados
+var edad = parseInt(prompt("Ingresa tu edad"));
 
 if(edad <= 12){
   alert("no puedes entrar");
@@ -27,28 +40,14 @@ if(edad <= 12){
   alert("Puedes entrar con un adulto");
 }else{
   alert("Puedes entrar");
-} */
-
-
-/* if(dinero < 5){
-  console.log("tienes menos de 5 pesos");
 }
-if(dinero == 5){
-  console.log("tienes 5 pesos");
 
-}
-if(dinero > 5){
-  console.log("tienes más de 5 pesos");
-} */
-
-
+//variables booleanas
 var salir = false;
 var comer = false;
 
 console.log(salir && comer);
 console.log(salir || !comer);
 
-
-
-(true && false && true) || false = false
-true || (false || false) = true
+console.log((true && false && true) || false)
+console.log(true || (false || false))
