1f203b0060388e4f8b6b361cea11db6a590732c1
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2021-12-08 12:15:14 -0600

    After 01

---
+++ After 01/main.js
@@ -0,0 +1,62 @@
+// comentario de una linea
+
+/* comentarios
+de 
+bloque 
+*/
+
+// //ENTRADA Y SALIDA DE DATOS 
+// alert()
+// prompt()
+// confirm()
+// console.log()
+
+// VARIABLES
+// - Distingue mayusculas y minusculas
+// -no empezar con numero
+// - se puede empezar con simbolos $ _ 
+// - nombre que identifique el dato que almacenamos
+
+// let nombre = "Agostina"
+// var nombre = "Agostina"
+// const NOMBRE = "Agostina"
+
+// let nombre;
+
+//STRING
+
+// let nombre = prompt("ingrese su nombre")
+// let apellido = prompt("ingrese su apellido")
+  
+// alert("Bienvenido: " + " " + nombre + " " + apellido)
+
+//Numeros
+
+// let num1 = Number(prompt("Ingrese un numero")) DECIMALES Y ENTEROS - NO ACEPTA STRINGS (NaN)
+// let num2 = Number(prompt("Ingrese otro numero"))
+// let num1 = parseInt(prompt("Ingrese un numero")) NUMEROS ENTEROS
+// let num2 = parseInt(prompt("Ingrese otro numero"))
+// let num1 = parseFloat(prompt("Ingrese un numero")) NUMEROS DECIMALES
+// let num2 = parseFloat(prompt("Ingrese otro numero"))
+
+// let suma = num1 + num2 
+// console.log(typeof(num1))
+
+// const PI =3.1416
+// console.log(PI.toFixed(3));
+
+// let num1 = Number(prompt("Ingrese un numero"))
+// console.log(Math.round(num1));
+
+// let num1 = Number(prompt("Ingrese un numero"))
+// let num2 = Number(prompt("Ingrese otro numero"))
+
+// let suma = num1+num2
+// let resta = num1-num2
+// let multiplicacion = num1*num2
+// let division = num1/num2
+
+// document.write("El resultado de la suma es" + " "+suma)
+// document.write("El resultado de la resta es" + " "+resta)
+// document.write("El resultado de la multiplicacion es" + " "+multiplicacion)
+// document.write("El resultado de la division es" + " "+division)
